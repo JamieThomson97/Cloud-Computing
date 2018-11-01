be2def84fc761a62393f1a8fd78ba72f7b927f0a
JamieThomson97 <JayTadmn@gmail.com>
2018-11-01 15:08:21 +0000

    formatting

--- Code/mapper.py
+++ Code/mapper.py
@@ -1,37 +1,40 @@
-#This script takes a an input of words as a .txt file, and returns a dictionary
-#The key's will be the words sorted lexicographically, values being being every occurrence of that anagram
+# This script takes a an input of words as a .txt file, and returns a dictionary
+# The key's will be the words sorted lexicographically, values being being every occurrence of that anagram
 
-FileName = "dict.txt" #A file containing thousands of random words
+FileName = "dict.txt"  # A file containing thousands of random words
 
-#Function separates each word into a dictionary the key being the word with value 1
 
-def allWords(FileName):
+# Function separates each word into a dictionary the key being the word with value 1
 
+def allWords(fileName):
     words = {}
-    with open(FileName) as f:
-            for line in f.readlines():
-                word = line.strip()
-                words[word] = 1
+    with open(fileName) as f:
+        for line in f.readlines():
+            word = line.strip()
+            words[word] = 1
     return words
 
-#Function returns a dictionary of every word sorted lexicographically, and the values are every permutation of said word that appears in the input
 
-def findAnagrams(words):
+# Function returns a dictionary of every word sorted lexicographically, and the values are every permutation of said word that appears in the input
+
+def find_anagrams(words):
+
     anagram_dict = {}
     for word in words:
 
-            #Sorts current word lexicographically
-            sortedWord = "".join(sorted(list(word)))
-            #If this word is not already in the dictionary, add it
-            if sortedWord not in anagram_dict:
-                anagram_dict[sortedWord] = []
-            #Append the current word(not sorted) to the set of values
-            anagram_dict[sortedWord].append(word)
+        # Sorts current word lexicographically
+        sortedWord = "".join(sorted(list(word)))
+        # If this word is not already in the dictionary, add it
+        if sortedWord not in anagram_dict:
+            anagram_dict[sortedWord] = []
+        # Append the current word(not sorted) to the set of values
+        anagram_dict[sortedWord].append(word)
     return anagram_dict
 
-#The above function returns every word, even if there is no anagram of it.
-#So the below function returns a new dictionary containing only key-value pairs with more than one value
-#I.E. more the sorted word has appeared more than once, so there must be an anagram of it
+
+# The above function returns every word, even if there is no anagram of it.
+# So the below function returns a new dictionary containing only key-value pairs with more than one value
+# I.E. more the sorted word has appeared more than once, so there must be an anagram of it
 
 def Anagrams(anagrams_dict):
     result = {}
@@ -39,20 +42,21 @@ def Anagrams(anagrams_dict):
     for pair in anagrams_dict:
         No_of_Values = len(anagrams_dict[pair])
         KeyLength = len(pair)
-        if No_of_Values > 1 and KeyLength > 2 :
+        if No_of_Values > 1 and KeyLength > 2:
             result[pair] = anagrams_dict[pair]
     return result
+
 
 RunWords = allWords(FileName)
 RunAnagrams = findAnagrams(RunWords)
 FilterAnagrams = Anagrams(RunAnagrams)
 
 for i in FilterAnagrams:
-    print (i)
-    print (FilterAnagrams[i])
-    print ("")
+    print(i)
+    print(FilterAnagrams[i])
+    print("")
 
-#Attempt to write Result dictionary to a file
+    # Attempt to write Result dictionary to a file
 
     FileName1 = "Dict1.txt"
     FileName2 = "Dict2.txt"
@@ -63,11 +67,11 @@ i = 0
 for Key, Value in FilterAnagrams.items():
 
     if (i % 2 == 0):
-        #print ("in even")
-        FileOpen1.write(str(Key) +' = '+ str(Value)+'\n')
+        # print ("in even")
+        FileOpen1.write(str(Key) + ' = ' + str(Value) + '\n')
     if (i % 2 != 0):
-        #print("in odd")
-        FileOpen2.write(str(Key) +' = '+ str(Value)+'\n')
+        # print("in odd")
+        FileOpen2.write(str(Key) + ' = ' + str(Value) + '\n')
     i += 1
 
 FileOpen1.close()
