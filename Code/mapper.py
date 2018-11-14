da2a0a415a3015fbbee6f252a74ffcba669fc02e
JamieThomson97 <j.thomson-15@student.lboro.ac.uk>
2018-11-14 20:02:52 +0000

    Works in Cloud

--- Code/mapper.py
+++ Code/mapper.py
@@ -1,54 +1,22 @@
+#!/usr/bin/env python
+
 # Takes input in form of text file and outputs list of key pairs
 # Key is word sorted lexicographically
 # Value is word
 
 import string
 import sys
-FileName = "dict.txt"
-count = 0
+
 punctuation = set(string.punctuation)
 
+word_pairs = []
 
-def make_list():
-    word_pairs = []
-    for line in sys.stdin.readlines():
-        remove_punct = ''.join(x for x in line if x not in punctuation)
-        line_no_punct = remove_punct.split()
+for line in sys.stdin:
+    remove_punct = ''.join(x for x in line if x not in punctuation)
+    line_no_punct = remove_punct.split()
 
-        for word in line_no_punct:
-            lower_word = word.lower()
-            sorted_word = "".join(sorted(list(lower_word)))
-            word_pairs.append((sorted_word+'\t'+lower_word))
-            print((sorted_word+'\t'+lower_word))
-            #print(sorted_word, "\t", lower_word)
-    return word_pairs
-
-
-result = make_list()
-
-#print(result)
-
-i = 0
-
-FileName1 = "Dict1.txt"
-FileName2 = "Dict2.txt"
-FileOpen1 = open(FileName1, "w")
-FileOpen2 = open(FileName2, "w")
-
-
-for line in result:
-
-    if i % 2 == 0:
-        # print ("in even")
-        FileOpen1.write(str(line)+"\n")
-
-    if i % 2 != 0:
-        # print("in odd")
-        FileOpen2.write(str(line)+"\n")
-
-    i += 1
-
-FileOpen1.close()
-FileOpen2.close()
-
-
+    for word in line_no_punct:
+        lower_word = word.lower()
+        sorted_word = "".join(sorted(list(lower_word)))
+        word_pairs.append((sorted_word+'\t'+lower_word))
+        print((sorted_word+'\t'+lower_word))
--- Code/reducer.py
+++ Code/reducer.py
@@ -1,32 +1,29 @@
+#!/usr/bin/env python
+
 # Takes the input as file, where each line a key value pair of a lexicographically sorted word as the key, and the actual word as the value
 # Outputs a list of every occurrence every anagram in the input
 
 
-filename = "Dict1.txt"
-fileopen = open(filename, 'r')
 
 import sys
-answer = {}
 
-i = 0
+anagram_pairs = {}
+
+
 for line in sys.stdin:
     words = line.split("\t")
+
     key = words[0].strip("\r\n")
-    #print(value)
     value = words[1].strip("\r\n")
-    if key not in answer:
-        answer[key] = []
 
-    answer[key].append(value)
+    if key not in anagram_pairs:
+        anagram_pairs[key] = []
+    if value not in [x for v in anagram_pairs.values() for x in v]:
 
+        anagram_pairs[key].append(value)
 
-FileName1 = "Mapped.txt"
+i = 0
 
-FileOpen1 = open(FileName1, "w")
-
-for i in answer:
-    if len(answer[i]) > 1:
-        FileOpen1.write(str(answer[i])+"\n")
-        print(str(answer[i])+"\n")
-
-
+for i in anagram_pairs:
+    if len(anagram_pairs[i]) > 1:
+        print(str(anagram_pairs[i])+"\"\n")
